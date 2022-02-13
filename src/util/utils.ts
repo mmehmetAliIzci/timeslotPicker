ace5d438fbcb8a3ca113d473abd7e4e210cf7ac4
Mehmet Ali IZCI <mmehmetaliizci@hotmail.com>
2022-02-13 15:35:06 +0100

    Nullish coalacent start / end time check for isTimeslotsEqual

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,5 +1,6 @@
 import {IParsedTimeslot} from "./groupTimeslotsByDay";
 
-export function isTimeslotsEqual (a: IParsedTimeslot, b: IParsedTimeslot):boolean {
-    return a.start_time.getTime() === b.start_time.getTime() && a.end_time.getTime() === b.end_time.getTime()
+export function isTimeslotsEqual (a?: IParsedTimeslot, b?: IParsedTimeslot):boolean {
+    return a?.start_time.getTime() === b?.start_time.getTime() && a?.end_time.getTime() === b?.end_time.getTime();
+
 }
