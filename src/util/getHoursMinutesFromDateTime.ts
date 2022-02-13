b10a74adf5cf0635e079f8912ad2be561628a498
Mehmet Ali IZCI <mmehmetaliizci@hotmail.com>
2022-02-13 13:00:04 +0100

    Handle errors for getHoursMin

--- src/util/getHoursMinutesFromDateTime.ts
+++ src/util/getHoursMinutesFromDateTime.ts
@@ -1,5 +1,18 @@
-export function getHoursMinutesFromDateTime(date: Date) {
-    const options: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit' }
-    let s = date.toLocaleDateString('en-GB',options);
-    return s.slice(s.indexOf(',') + 1, s.length)
+import {locale} from "./constants";
+
+// Converts "10/02/2022, 08:10" to 08:10
+export function getHoursMinutesFromDateTime(date: Date | undefined) {
+    try {
+        if (date) {
+            const options: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit', hourCycle: "h24" }
+            let s = date.toLocaleDateString(locale,options);
+            return s.slice(s.indexOf(',') + 1, s.length)
+        }
+        return "--:--"
+    }
+    catch (e) {
+        console.error('Something went wrong while getting hours and minutes from DateTime');
+        return "--:--"
+    }
+
 }
